d5e24970cb2894bb2dccdf16b4c0a71195e45fda
tturpin <tturpin@student.42.fr>
2024-11-29 17:17:51 +0100

    debut_main

--- header/minishell.h
+++ header/minishell.h
@@ -3,30 +3,37 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cballet <cballet@student.42.fr>            +#+  +:+       +#+        */
+/*   By: tturpin <tturpin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/26 14:41:34 by tturpin           #+#    #+#             */
-/*   Updated: 2024/11/26 15:39:01 by cballet          ###   ########.fr       */
+/*   Updated: 2024/11/29 17:15:51 by tturpin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
+# include <errno.h>
+# include <fcntl.h>
+# include <readline/history.h>
+# include <readline/readline.h>
+# include <signal.h>
 # include <stdbool.h>
 # include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <sys/types.h>
+# include <sys/wait.h>
 # include <unistd.h>
 
 typedef struct s_cmd
 {
-	char			**cmd;/*cmd[0] = commande et cmd[1 ou plus] = arguments*/
+	char			**cmd;
 	int				fd_input;
 	int				fd_output;
 	bool			pipe;
 	struct s_cmd	*next;
 	struct s_cmd	*prev;
 }					t_cmd;
-
-
 
 #endif
--- src/minishell.c
+++ src/minishell.c
@@ -1,12 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   minishell.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tturpin <tturpin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/26 11:21:58 by tturpin           #+#    #+#             */
-/*   Updated: 2024/11/29 17:06:25 by tturpin          ###   ########.fr       */
+/*   Updated: 2024/11/29 17:17:35 by tturpin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../header/minishell.h"
+
+int	main(void)
+{
+	char	*input;
+
+	while (1)
+	{
+		input = readline("minishell> ");
+		if (input == NULL)
+		{
+			printf("Bye!\n");
+			break ;
+		}
+		// ft_parsing(input);
+		// printf("Vous avez tapé : %s\n", input);
+		free(input);
+	}
+	return (0);
+}
